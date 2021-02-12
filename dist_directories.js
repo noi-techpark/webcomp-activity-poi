e3cdf8706e1995060022a55511664b0b3359185f
davide <d@vide.bz>
2021-02-12 17:53:30 +0100

    document.currentScript.src to have a base url

--- dist_directories.js
+++ dist_directories.js
@@ -1,8 +1,17 @@
+
+
+let bundle_script_src = document.currentScript.src
+
+// remove bundle.js
+bundle_script_src = bundle_script_src.substring(0, bundle_script_src.length - 'bundle.js'.length)
+
+// alert('>' + bundle_script_src + '<')
+
 window.paths = {
-    css: "",
-    css_components: "",
-    img: "",
-    img_category_icons: "",
-    img_map_markers: "",
-    data:""
+    css: bundle_script_src,
+    css_components: bundle_script_src,
+    img: bundle_script_src,
+    img_category_icons: bundle_script_src,
+    img_map_markers: bundle_script_src,
+    data: bundle_script_src
 };
