5d3b4a5567b87a9374b0b6c82b3b3a84832be745
Giacomo92 <info@giacomofabbian.it>
2026-01-15 11:43:39 +0100

    fix: marker visualization

--- src/InteractiveMapComponent.js
+++ src/InteractiveMapComponent.js
@@ -106,6 +106,14 @@ class InteractiveMapComponent extends HTMLElement
 
 		setTimeout(function()
 			{
+				// Fix Leaflet default icon paths
+				delete L.Icon.Default.prototype._getIconUrl;
+				L.Icon.Default.mergeOptions({
+					iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+					iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+					shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png'
+				});
+
 				let map = L.map(mapdiv, { zoomControl: false })
 				thiswebcomponent.map = map
 
